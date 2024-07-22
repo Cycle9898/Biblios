5d468ad427be8214c31074b0bc361a238fee48b1
Cycle9898 <cycle9898@gmail.com>
2024-07-22 17:48:02 +0200

    adding admin form for new books + fixing padding bottom on admin new pages

--- src/Controller/Admin/BookController.php
+++ src/Controller/Admin/BookController.php
@@ -2,34 +2,34 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Editor;
-use App\Form\EditorType;
+use App\Entity\Book;
+use App\Form\BookType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin/editor')]
-class EditorController extends AbstractController
+#[Route('/admin/book')]
+class BookController extends AbstractController
 {
-    #[Route('', name: 'app_admin_editor_index', methods: ['GET'])]
+    #[Route('', name: 'app_admin_book_index', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('admin/editor/index.html.twig', []);
+        return $this->render('admin/book/index.html.twig', []);
     }
 
-    #[Route('/new', name: 'app_admin_editor_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_admin_book_new', methods: ['GET', 'POST'])]
     public function new(Request $request): Response
     {
-        $editor = new Editor();
-        $form = $this->createForm(EditorType::class, $editor);
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             //TODO
         }
 
-        return $this->render('admin/editor/new.html.twig', [
+        return $this->render('admin/book/new.html.twig', [
             'form' => $form
         ]);
     }
