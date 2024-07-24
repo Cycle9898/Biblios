b99f52b2c1f3886b70ca957a296f625d2cff153e
Cycle9898 <cycle9898@gmail.com>
2024-07-24 17:20:20 +0200

    adding pagination for author list in admin view

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -6,6 +6,8 @@ use App\Entity\Author;
 use App\Form\AuthorType;
 use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,7 +28,11 @@ class AuthorController extends AbstractController
             $dates['end'] = $request->query->get('end');
         }
 
-        $authors = $authorRepo->findByDateOfBirth($dates);
+        $authors = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new QueryAdapter($authorRepo->findByDateOfBirth($dates)),
+            $request->query->get('page', 1),
+            10
+        );
 
         return $this->render('admin/author/index.html.twig', [
             'authors' => $authors
