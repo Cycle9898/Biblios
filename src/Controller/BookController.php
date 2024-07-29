536cce93cd75d321f3075cc8ab651c539a51ad85
Cycle9898 <cycle9898@gmail.com>
2024-07-29 17:26:34 +0200

    adding book info page on public view + changing date format on all views

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Pagerfanta\Pagerfanta;
@@ -24,6 +25,14 @@ class BookController extends AbstractController
 
         return $this->render('book/index.html.twig', [
             'books' => $books
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_book_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(?Book $book): Response
+    {
+        return $this->render('book/show.html.twig', [
+            'book' => $book
         ]);
     }
 }
